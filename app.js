e087ff09143c9b130179f7305714231c8098f57a
kapadiamush <kapadiamush@gmail.com>
2016-12-24 15:14:57 -0800

    initial skeleton

---
+++ app.js
@@ -0,0 +1,41 @@
+'use strict;'
+
+const express = require('express');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    .json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+else {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500)
+    .json({
+      message: err.message,
+      error: {}
+    });
+  });
+}
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict;'
+
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
